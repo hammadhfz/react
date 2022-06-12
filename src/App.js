e067cc643e501642a325bc67087914ced7c6626d
Malik Hammad Hafeez <hafeezhammad34@gmail.com>
2022-06-13 01:22:11 +0500

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
-import logo from "./logo.svg";
-import "./App.css";
-let name = "Hammad";
-let a = 78;
-function formatName(obj) {
-  return obj.firstName + " " + obj.lastName;
-}
-
-const user = {
-  firstName: "Harper",
-  lastName: "Perez",
-};
-
-const element = <h3>Hello, {formatName(user)}!</h3>;
 
 function App() {
   return (
     <>
-      <div className="container">
-        <h1>UI/Ux</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-          praesentium explicabo quasi illum maxime vitae corporis minima
-          ducimus! Saepe, error doloribus eaque natus quae adipisci at
-          consectetur debitis impedit ratione?
-        </p>
-        <p>
-          {name}
-          {a} feel the magic in the air
-        </p>
-        <p>{element}</p>
+    <nav className="navbar navbar-expand-lg bg-secondary">
+      <div className = "container-fluid ">
+        <a className="navbar-brand text-light" href="/">Navbar</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active text-light" aria-current="page" href="/">Home</a>
+            </li>
+            <li li className = "nav-item" >
+              <a className="nav-link text-light" href="/">Link</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled text-dark">Disabled</a>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+            <button className="btn btn-dark" type="submit">Search</button>
+          </form>
+        </div>
       </div>
-    </>
+    </nav>
+  </>
   );
 }
 
